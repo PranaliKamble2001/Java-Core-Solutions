83ee264b6e963ce6bfcc6f45185e5998f227cf6d
PranaliKamble2001 <134600785+PranaliKamble2001@users.noreply.github.com>
2026-03-06 22:04:51 +0530

    feat: implement array element accumulation logic

---
+++ src/ArrayPrograms/SumArray.java
@@ -0,0 +1,24 @@
+package ArrayProgram;
+
+import java.util.Arrays;
+
+public class SumArray 
+{
+   public static void main(String [] args)
+   {
+//	  
+	   int[] arr = {10, 20, 30, 40, 50};
+       
+       //  variable initialized to zero
+       int sum = 0;
+
+       // Traverse the array and add each element to sum
+       for (int i = 0; i < arr.length; i++) {
+           sum += arr[i]; // Same as: sum = sum + arr[i]
+       }
+
+       System.out.println("Array Elements: " + Arrays.toString(arr));
+       System.out.println("Total Sum of Elements: " + sum);
+   }
+
+}
