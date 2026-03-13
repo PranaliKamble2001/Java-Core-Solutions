e63b1683d403217f7dd75bda02571e3ead095d3c
PranaliKamble2001 <134600785+PranaliKamble2001@users.noreply.github.com>
2026-03-13 17:38:44 +0530

    feat: add MatrixTranspose logic to flip rows and columns

---
+++ src/MatrixPrograms/MatrixTranspose.java
@@ -0,0 +1,34 @@
+package MatrixPrograms;
+
+public class MatrixTranspose {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transpose = new int[cols][rows];
+
+        // Logic: Swap row index with column index
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        System.out.println("--- Transposed Matrix ---");
+        printMatrix(transpose);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
