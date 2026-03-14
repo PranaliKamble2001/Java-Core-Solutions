402bf9cc0b5bae83f8b6a5f9821f173972041f06
PranaliKamble2001 <134600785+PranaliKamble2001@users.noreply.github.com>
2026-03-14 15:57:41 +0530

    feat: add UpperTriangle logic for matrix diagonal manipulation

---
+++ src/MatrixPrograms/UpperTriangle.java
@@ -0,0 +1,23 @@
+package MatrixPrograms;
+
+public class UpperTriangle {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Upper Triangular Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i <= j) {
+                    System.out.print(matrix[i][j] + " ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
