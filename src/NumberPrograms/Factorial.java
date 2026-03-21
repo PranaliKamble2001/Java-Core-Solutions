f6ca0edf9b7abd429bb94641508f9e3d4310f90c
PranaliKamble2001 <134600785+PranaliKamble2001@users.noreply.github.com>
2026-03-21 14:08:06 +0530

    feat: add Factorial logic with iterative implementation

---
+++ src/NumberPrograms/Factorial.java
@@ -0,0 +1,13 @@
+package NumberPrograms;
+
+public class Factorial {
+    static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1); // Recursive call
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is: " + factorial(num));
+    }
+}
